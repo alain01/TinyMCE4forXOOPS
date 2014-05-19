ca5e120ad4dfe8c3e6d78abdfa3b3e2d363fbfd4
ggoffy <gottfried.gabor@gmail.com>
2014-05-19 12:46:11 +0200

    added language files to plugin xoops_tagextgal

---
+++ htdocs/class/xoopseditor/tinymce4/external_plugins/xoops_tagextgal/langs/en.js
@@ -0,0 +1,8 @@
+// updated for TinyMCE v4 / 2014-05-17 / by goffy
+
+tinyMCE.addI18n('en.xoops_tagextgal',{
+    _XOOPS_EDITOR_TINYMCE_TAG_EXTGAL_TOOLTIP:"Tag eXtGallery",
+    _XOOPS_EDITOR_TINYMCE_TAG_EXTGAL_TITLE:"Plugin Tag eXtGallery",
+    _XOOPS_EDITOR_TINYMCE_TAG_EXTGAL_LABEL_PHOTO:"Title of photo",
+    _XOOPS_EDITOR_TINYMCE_TAG_EXTGAL_LABEL_ID:"Id eXtGallery of the photo"
+});
